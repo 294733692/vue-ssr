e5cff6e12179287cba36079df94827d9b2ede14c
yuyunchao <294733692@qq.com>
2018-08-22 15:45:38 +0800

    注释

--- SSR-sec/entry/entry-server.js
+++ SSR-sec/entry/entry-server.js
@@ -1,6 +1,7 @@
 import { createApp } from '../src/main'
 
 export default () => {
+    // 返回promise对象，用于处理异步状态
     return new Promise((resolve, reject) => {
         let app = createApp();
         resolve(app);
